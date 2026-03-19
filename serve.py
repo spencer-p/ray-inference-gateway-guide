2464b5434b965b5c5ef854ae2b340713beafde94
Spencer Peterson <spencerjp@google.com>
2026-03-19 20:38:46 +0000

    switch to pythonic config

--- serve.py
+++ serve.py
@@ -2,14 +2,13 @@ import logging
 from typing import List, Optional
 
 from ray.llm._internal.serve.core.protocol import RawRequestInfo
-from ray.serve._private.constants import SERVE_LOGGER_NAME
 from ray.serve._private.request_router.common import PendingRequest
 from ray.serve._private.request_router.pow_2_router import PowerOfTwoChoicesRequestRouter
 from ray.serve._private.request_router.replica_wrapper import RunningReplica
 from ray.util.annotations import PublicAPI
 from ray.serve.llm import build_openai_app
 
-logger = logging.getLogger(SERVE_LOGGER_NAME)
+logger = logging.getLogger(__name__)
 
 class InferenceGatewayRequestRouter(PowerOfTwoChoicesRequestRouter):
     """Request router that routes to a specific backend IP if a header is present.
@@ -23,7 +22,7 @@ class InferenceGatewayRequestRouter(PowerOfTwoChoicesRequestRouter):
         candidate_replicas: List[RunningReplica],
         pending_request: Optional[PendingRequest] = None,
     ) -> List[List[RunningReplica]]:
-        logger.info(f"routing request for {pending_request}")
+        logger.info(f"igw: routing request for {pending_request}")
         if not pending_request:
           return []
 
@@ -31,25 +30,19 @@ class InferenceGatewayRequestRouter(PowerOfTwoChoicesRequestRouter):
         raw_request_info = None
         for arg in pending_request.args:
             if isinstance(arg, RawRequestInfo):
-                logger.info("raw_request_info found in pending_request.args")
                 raw_request_info = arg
                 break
 
-        if not raw_request_info and "raw_request_info" in pending_request.kwargs:
-            logger.info("raw_request_info found in kwargs")
-            raw_request_info = pending_request.kwargs["raw_request_info"]
-
         if not raw_request_info:
-            logger.warning("no raw request info found")
+            logger.warning("igw: No request metadata is available; cannot route from headers. Falling back.")
             return await super().choose_replicas(candidate_replicas, pending_request)
 
         target_endpoint = raw_request_info.headers.get(
             "x-gateway-destination-endpoint"
         )
         if not target_endpoint:
-            logger.warning(f"no gateway endpoint found in {raw_request_info.headers}")
+            logger.warning(f"igw: no gateway endpoint found in {raw_request_info.headers}")
             return await super().choose_replicas(candidate_replicas, pending_request)
-        logger.info(f"igw provided hint to route to {target_endpoint}")
 
         # Header is "ip:port", we only care about IP.
         target_ip = target_endpoint.split(":")[0]
@@ -61,15 +54,17 @@ class InferenceGatewayRequestRouter(PowerOfTwoChoicesRequestRouter):
 
         if not filtered_replicas:
             logger.warning(
-                f"Request specified target IP {target_ip} via header, "
+                f"igw: Request specified target IP {target_ip} via header, "
                 "but no matching replicas were found. Falling back to default routing."
             )
             return await super().choose_replicas(candidate_replicas, pending_request)
 
         if len(filtered_replicas) == 1:
+          logger.info(f"igw: requested endpoint {target_ip} can only be fulfilled by {filtered_replicas[0].replica_id}")
           return [filtered_replicas]
 
         # Run power of two choices with the filtered replicas.
+        logger.info(f"igw: requested endpoint {target_ip} has {len(filtered_replicas)} target replicas, falling back")
         return await super().choose_replicas(
             filtered_replicas, pending_request
         )
